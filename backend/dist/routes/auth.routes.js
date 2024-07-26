72a970ac3e744f5683d30e3fd1c0867a93898f57
Tabare Bermudez <tabarebermudez@gmail.com>
2024-07-26 13:08:03 -0300

    docs(backend): :chart_with_upwards_trend: Update swagger doc

--- backend/dist/routes/auth.routes.js
+++ backend/dist/routes/auth.routes.js
@@ -13,10 +13,35 @@ exports.default = express_1.default
    * POST /api/v1/auth/register
    * @summary Register new users
    * @tags AutH
-   * @param {array<object>} request.body.required - User details
-   * @return {array<object>} 201 - User created
+   * @param {object} request.body.required - User details
+   * @example request - Example of request body
+   * {
+   *    "username": "admin",
+   *    "email": "admin@admin.com",
+   *    "password": "admin1234"
+   * }
+   * @return {object} 201 - User created
+   * @example response - 201 - Example of response
+   * {
+   *  "success": true,
+   *  "message": "User created successfully.",
+   *  "user": {
+   *     "username": "admin"
+   *    },
+   *  "token": "eyJhbGciOiJIUzI1NiIsInR5..."
+   * }
    * @return {object} 400 - Invalid data
+   * @example response - 400 - Example of response
+   * {
+   *  "success": false,
+   *  "message": "Error: Registration -> email must be unique."
+   * }
    * @return {object} 500 - Internal server error
+   * @example response - 500 - Example of response
+   * {
+   *  "success": false,
+   *  "message": "Internal server error."
+   * }
    */
     .post("/register", auth_controller_1.default.register)
     /**
@@ -24,9 +49,33 @@ exports.default = express_1.default
      * @summary Login users
      * @tags AutH
      * @param {object} request.body.required - User details
+     * @example request - Example of request body
+     * {
+     *    "email": "admin@admin.com",
+     *    "password": "admin1234"
+     * }
      * @return {object} 200 - User logged in
+     * @example response - 200 - Example of response
+     * {
+     *  "success": true,
+     *  "message": "User logged in successfully.",
+     *  "user": {
+     *     "username": "admin"
+     *    },
+     *  "token": "eyJhbGciOiJIUzI1NiIsInR5..."
+     * }
      * @return {object} 400 - Invalid data
+     * @example response - 400 - Example of response
+     * {
+     *  "success": false,
+     *  "message": "Invalid email or password."
+     * }
      * @return {object} 500 - Internal server error
+     * @example response - 500 - Example of response
+     * {
+     *  "success": false,
+     *  "message": "Internal server error."
+     * }
      */
     .post("/login", auth_controller_1.default.login)
     .use(auth_mid_1.default.authenticate("userJWT", { session: false }))
